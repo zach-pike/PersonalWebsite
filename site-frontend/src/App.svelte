458c4e58dab0e3a9d0f726c9b9c81342baaabba6
Zachary Pike <zachturnpike@gmail.com>
2025-07-27 10:41:39 -0400

    Getting started

--- site-frontend/src/App.svelte
+++ site-frontend/src/App.svelte
@@ -1,47 +1,23 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="bg-slate-700 w-screen h-screen flex flex-col items-center p-4">
+  <p class="w-full md:w-3/4 text-white text-3xl m-2">zpike.net</p>
+  <div class="w-full md:w-3/4 flex-col sm:flex-row flex gap-1 ">
+
+    <div class="p-4 sm:w-3/4 w-full h-full bg-white">
+      <p class="text-2xl">Welcome!</p>
+      <p>Hello, my name is Zachary and this is my website! I live on the eastern shore of Maryland, near Kent Island. My hobbies include; Computing, Radio, Engineering, and of course Programming. I am interested in pursuing a Computer Science Degree from Salisbury Univerity, for now I work at a marina doing boat related stuff.</p>
+    </div>
+
+    <div class="p-4 sm:w-1/4 w-full h-full bg-white flex flex-col items-center">
+      <p>Navigation</p>
+
+      <ul class="underline">
+        <li><a href="https://github.com/zach-pike">My Github</a></li>
+        <li><a href="#">Go to a random project</a></li>
+      </ul>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+</div>
---
+++ test.md
@@ -0,0 +1,5 @@
+website ideas
+
+ - [] project list page
+ - [] blog page
+ - [] last spotify listen
